69f40baccd44c62efa2f93fb2854838714d505de
dim912 <dim912@gmail.com>
2020-11-11 01:12:02 +0800

    init

---
+++ src/main/java/com/example/uploadingfiles/storage/FileSystemStorageService.java
@@ -0,0 +1,38 @@
+package com.example.uploadingfiles.storage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.example.uploadingfiles.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileSystemStorageService implements StorageService {
+
+	private final Path rootLocation;
+
+	@Autowired
+	public FileSystemStorageService(StorageProperties properties) {
+		this.rootLocation = Paths.get(properties.getLocation());
+	}
+
+	@Override
+	public void store(List<User> userList) {
+
+
+	}
+}
+
